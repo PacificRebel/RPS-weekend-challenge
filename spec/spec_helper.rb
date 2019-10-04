d2d7062d4a51f71a876d474d7f4c898faee8ed5e
Heli Sivunen <heli@mooncat.tv>
2019-10-04 11:06:15 +0100

    Add two name forms on home page with associated tests passing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+# require 'simplecov'
+# require 'simplecov-console'
+#
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   # Want a nice code coverage website? Uncomment this next line!
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -5,7 +15,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-# require 'features/web_helpers'
+require 'features/web_helpers'
 
 Capybara.app = RockPaperScissors
 
